aed49bc8fd86a74ed1cdf44359655e61fe38d497
unknown <Administrator@MCGD-TRN013.COMVIVA.COM>
2020-02-18 12:13:49 +0530

    SAGRIKA

---
+++ src/main/java/com/via/springbootweb/Course.java
@@ -0,0 +1,53 @@
+package com.via.springbootweb;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQuery(name="findAllCourses", query="from Course e")
+public class Course {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	int id;
+	
+	
+	String title;	
+	String summary;
+	public Course(){}
+	public Course(String title, String summary) {
+
+		this.title = title;
+		this.summary = summary;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", title=" + title + ", summary=" + summary + "]";
+	}
+	
+	
+	
+
+}
